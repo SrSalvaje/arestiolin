def3f55c7d9288f6025f22da573e925761320739
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-03-26 22:58:49 +0100

    feat: styled Profile

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import styles from './Profile.module.scss';
+import Widget from '../Widget/Widget';
 
 const Profile = () => (
-    
+
     <div className={styles.main}>
+      <Widget
+                class={styles.gridPos1} 
+                title={"Jon Miren Aresti Olin"} 
+                content={<ul >
+                    <li>30 years old (22/03/1988), Spanish and Mexican Citizenship</li>
+                    <li>Swedish Personal Number: 880322-1251</li>
+                    <li>Front-end Web Developer</li>
+                    <li>Journalist</li>
+                    <li>Video Editor</li>
+                    <li>PADI Certified SCUBA diving instructor</li>
+                    <li>Email: jon.m.h.aresti@gmail.com</li>
+                    <li>cellphone: 07 6321 82 80</li>
+                </ul>} 
+                />
+
 
      
     </div>
