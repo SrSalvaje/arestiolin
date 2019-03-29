eb15c59b5679364a120d2f53ee2c0951f7be083c
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-03-29 17:35:13 +0100

    feat: totally unnecesarry evrtiocal alignemt

--- src/Components/Portfolio/Carousel/Carousel.js
+++ src/Components/Portfolio/Carousel/Carousel.js
@@ -8,11 +8,11 @@ import styles from "./Carousel.module.scss";
 const Carousel =(props)=>(
 
   <div className={styles.main}>
-    <div className={styles.left}>{props.img1} </div>
-    <div className={styles.center}>{props.img2} </div>
-    <div className={styles.right}>{props.img3} </div>
-    <div className={styles.prev} >&#10094;</div>
-    <div className={styles.next} >&#10095;</div>
+     <div className={styles.topR}>{props.img1} </div> 
+    <div className={styles.centerR}>{props.img2} </div>
+    <div className={styles.bottomR}>{props.img3} </div> 
+   {/*  <div className={styles.prev} >&#10094;</div>
+    <div className={styles.next} >&#10095;</div> */}
   </div>
   
 
--- src/Components/Portfolio/ProjectWidget/ProjectWidget.js
+++ src/Components/Portfolio/ProjectWidget/ProjectWidget.js
@@ -8,15 +8,16 @@ import PropTypes from 'prop-types';
 const ProjectWidget =(props)=>(
   <div className={styles.main}>
     <h3>{props.title}</h3>
-    <div className={styles.description}>
-        {props.description}
-    </div>
-    <div className={styles.leftColumn}>
+    <div className={styles.projectImg}>
         <div className={styles.imgCont}>
             <img class={styles.img} src={props.image} alt={props.alt}/>  
         </div>
+    </div>
+    <div className={styles.description}>
+        {props.description}
         <button className={styles.btn}>Open in new window</button>
     </div>
+    
     
   </div>
   
