7fc37372cf9124e626dc712531a88f550f5aa6ca
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-03-29 10:44:29 +0100

    feat: created carousel startfiles

---
+++ src/Components/Portfolio/Carousel/Carousel.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./Carousel.module.scss";
+
+
+
+
+
+const Carousel =(props)=>(
+
+  <div className={styles.main}>
+   
+
+    
+
+  </div>
+  
+
+);
+
+
+
+
+export default Carousel;
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from "./Portfolio.module.scss";
-import ProjectWidget from './ProjectWidget/ProjectWidget'
+import ProjectWidget from './ProjectWidget/ProjectWidget';
+import Carousel from './Carousel/Carousel';
 import PropTypes from 'prop-types';
 
 
@@ -9,12 +10,14 @@ import PropTypes from 'prop-types';
 const Portfolio =(props)=>(
   <div className={styles.main}>
     <h2>{props.title? props.title:"Title goes here"}</h2>
-    <ProjectWidget
+    <Carousel/>
+
+    {/* <ProjectWidget
         title={"Place Holder"}
         description={"ipsolorem something lorem ipsp very long, much interestinmg ö<jklsaedfghkjkfdagkjhdfgszkhjbfdgs"}
         image={'http://via.placeholder.com/604x611'}
         alt={'ipso ipso'}
-    />
+    /> */}
 
   </div>
   
