d26d89bd3ca3069cd6527e12035b0330a8e40efd
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-03-29 10:59:06 +0100

    feat: hardcoded images into carousel

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -10,7 +10,30 @@ import PropTypes from 'prop-types';
 const Portfolio =(props)=>(
   <div className={styles.main}>
     <h2>{props.title? props.title:"Title goes here"}</h2>
-    <Carousel/>
+    <Carousel
+      img1={<ProjectWidget
+        title={"img1"}
+        description={"ipsolorem something lorem ipsp very long, much interestinmg ö<jklsaedfghkjkfdagkjhdfgszkhjbfdgs"}
+        image={'http://via.placeholder.com/604x611'}
+        alt={'ipso ipso'}
+      />}
+
+      img2={<ProjectWidget
+        title={"img2"}
+        description={"ipsolorem something lorem ipsp very long, much interestinmg ö<jklsaedfghkjkfdagkjhdfgszkhjbfdgs"}
+        image={'http://via.placeholder.com/604x611'}
+        alt={'ipso ipso'}
+      />}
+
+      img3={
+        <ProjectWidget
+        title={"img3"}
+        description={"ipsolorem something lorem ipsp very long, much interestinmg ö<jklsaedfghkjkfdagkjhdfgszkhjbfdgs"}
+        image={'http://via.placeholder.com/604x611'}
+        alt={'ipso ipso'}
+    />
+      }
+    />
 
     {/* <ProjectWidget
         title={"Place Holder"}
