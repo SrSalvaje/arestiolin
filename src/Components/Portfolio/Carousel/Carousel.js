b1e3a8ffc782a206ff8260c700106cf6c399e3ea
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-01 10:04:33 +0200

    feat: added static controls

--- src/Components/Portfolio/Carousel/Carousel.js
+++ src/Components/Portfolio/Carousel/Carousel.js
@@ -11,8 +11,8 @@ const Carousel =(props)=>(
      <div className={styles.leftC}>{props.img1} </div> 
     <div className={styles.centerC}>{props.img2} </div>
     <div className={styles.rightC}>{props.img3} </div> 
-   {/*  <div className={styles.prev} >&#10094;</div>
-    <div className={styles.next} >&#10095;</div> */}
+    <div className={styles.prev} >&#10094;</div>
+    <div className={styles.next} >&#10095;</div>
   </div>
   
 
