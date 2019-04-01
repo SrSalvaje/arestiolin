a7721d9d6d586a5267473c740c59e7fa76bf9851
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-01 22:53:43 +0200

    feat: added event listener

--- src/Components/CV/List/List.js
+++ src/Components/CV/List/List.js
@@ -5,11 +5,12 @@ const List = (props) => (
     <div className={styles.listCont}>
         <h2>{props.listTitle}</h2>
         <ul className={styles.employmentList}>
-        {Object.keys(props.items).map((key, index)=>(<li key={`job${index}`} className={styles.listItem}>{props.items[key].title}</li> ))}
+        {Object.keys(props.items).map((key)=>(<li onClick={(ev)=>props.openInfo(ev.target)} id= {props.items[key].id} key={props.items[key].id} className={styles.listItem}>{props.items[key].title}</li> ))}
         </ul>
 
     </div>
        
+      
 
 
     
