0b7ffbabed8e59dc320e3790b885d7ffa29462d0
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-01 09:53:32 +0200

    feat: stacked carousel imgs

--- src/Components/Portfolio/Carousel/Carousel.js
+++ src/Components/Portfolio/Carousel/Carousel.js
@@ -8,9 +8,9 @@ import styles from "./Carousel.module.scss";
 const Carousel =(props)=>(
 
   <div className={styles.main}>
-     <div className={styles.topR}>{props.img1} </div> 
-    <div className={styles.centerR}>{props.img2} </div>
-    <div className={styles.bottomR}>{props.img3} </div> 
+     <div className={styles.leftC}>{props.img1} </div> 
+    <div className={styles.centerC}>{props.img2} </div>
+    <div className={styles.rightC}>{props.img3} </div> 
    {/*  <div className={styles.prev} >&#10094;</div>
     <div className={styles.next} >&#10095;</div> */}
   </div>
