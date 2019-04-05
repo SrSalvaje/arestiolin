dabd0cd5ca6471aa86e1a8d8e2638d22084b4e37
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-05 10:09:03 +0200

    feat: added background to app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import HeroCont from './Components/Hero/HeroCont';
 import Profile from './Components/Profile/Profile';
 import CV from './Components/CV/CV';
 import Portfolio from './Components/Portfolio/Portfolio';
-import Nav from './Components/Nav/Nav'
+import Nav from './Components/Nav/Nav';
+import bc from './Assets/img/hero.JPG';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div style={{ backgroundImage: `url( ${bc} )`}}></div>
         <HeroCont
           title={content.hero.title}
           content={content.hero.content}
