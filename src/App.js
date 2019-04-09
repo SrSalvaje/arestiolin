2bac7442e1773907cfb8f923f72653e769ba9427
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-09 13:50:04 +0200

    feat: added GetScroll Hoc

--- src/App.js
+++ src/App.js
@@ -7,29 +7,40 @@ import CV from './Components/CV/CV';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Nav from './Components/Nav/Nav';
 import bc from './Assets/img/hero.JPG';
+import GetScrollPos from './HOCS/GetScrollPos/GetScrollPos';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div style={{ backgroundImage: `url( ${bc} )`}}></div>
-        <HeroCont
-          title={content.hero.title}
-          content={content.hero.content}
-          button={content.hero.button}
-          background={content.hero.background}
-        />
-       {/*  <Nav
-          links={[{name:'Profile', position: 1 },
-          {name:'CV', position: 2 },
-          {name:'Portfolio', position: 3 }
-          ]}
-        /> */}
-        <Profile/>
-        <CV/>
-        <Portfolio/>
-        
-      </div>
+   
+        <div className="App">
+          <div style={{ backgroundImage: `url( ${bc} )`}} className="heroImg"></div>
+          <GetScrollPos>
+            <HeroCont
+              title={content.hero.title}
+              content={content.hero.content}
+              button={content.hero.button}
+              background={content.hero.background}
+              scrollAt={100}
+            />
+            
+            <Nav
+              links={[{name:'Profile', position: 1 },
+              {name:'CV', position: 2 },
+              {name:'Portfolio', position: 3 }
+              ]}
+              scrollAt={100}
+            />
+            <Profile
+              scrollAt={60}/>
+            <CV
+            scrollAt={80}
+            />
+            <Portfolio
+            scrollAt={50}/>
+          </GetScrollPos> 
+        </div>
+    
     );
   }
 }
