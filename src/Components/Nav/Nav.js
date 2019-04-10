405de79222a3e771dba65ba0cb05c507b4cdb6b2
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-10 21:20:47 +0200

    feat: nav shows based on hero position

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,11 +6,10 @@ import styles from "./Nav.module.scss";
 
 
 const Nav = React.forwardRef((props, ref)=>(
-    <ul className={styles.main} ref={ref}>
+    <ul className={(props.verticalPosition<=-80 )?styles.main: styles.hide} ref={ref}>
         {props.links.map(link=>(
             <li key={link.name} className={styles.links} onClick={()=>{
                 window.scrollTo(0,`${(window.innerHeight+100)*link.position}`); 
-                console.log("hi");
             }}>{link.name}</li>
         ))}
     </ul>
