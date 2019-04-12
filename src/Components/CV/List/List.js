6102c644ffc44a489911c92c94db1b5bddf384f6
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-12 14:59:57 +0200

    feat: style based on position

--- src/Components/CV/List/List.js
+++ src/Components/CV/List/List.js
@@ -2,8 +2,7 @@ import React from 'react';
 import styles from './List.module.scss';
 
 const List = (props) => (
-    <div className={styles.listCont}>
-        <h2>{props.listTitle}</h2>
+    <div className={props.position<=350? styles.displayList : styles.listCont }>
         <ul className={styles.employmentList}>
         {Object.keys(props.items).map((key)=>(<li onClick={(ev)=>props.openInfo(ev.target)} id= {props.items[key].id} key={props.items[key].id} className={styles.listItem}>{props.items[key].title}</li> ))}
         </ul>
