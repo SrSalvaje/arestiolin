f022c117ac6eaca94873195e41c7b6729f55b8b1
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-12 14:59:38 +0200

    feat: removed commented out code

--- src/HOCS/GetScrollPos/GetScrollPos.js
+++ src/HOCS/GetScrollPos/GetScrollPos.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import _, {debounce}from 'lodash';
-import variables from '../../Assets/_variables.module.scss'
+import  {debounce}from 'lodash';
+//import variables from '../../Assets/_variables.module.scss'
 
 
 class GetScrollPos extends Component {
@@ -12,7 +12,6 @@ class GetScrollPos extends Component {
             views:[],
             navBarheight:0
         };
-        //React.Children.forEach(this.props.children, (child)=>{console.log(child)})
         //for each child 
         this.props.children.forEach((child, index)=>{
           //initializes a state for each children, this will be used to know if a ref has gone over the height I want
@@ -32,60 +31,21 @@ class GetScrollPos extends Component {
     
     
     getPosition=()=>{
-        const viewPortHeight= window.innerHeight;
-        const viePortWithNav=viewPortHeight-this.state.navBarheight;
         const copyOfViews=this.state.views.slice(0);
       
-      //console.log(this.state.view0.addRef.current.getBoundingClientRect().y);
-       //console.log(this.state.view1.addRef.current.getBoundingClientRect()) 
-       console.log(this.state.view2.addRef.current.getBoundingClientRect().y)
-       //console.log(this.state.view2.addRef.current.clientHeight)
-        //console.log(this.state.view3.addRef.current.offsetTop)
-       //console.log(this.state.view4.addRef.current.offsetTop)
-       // get height of element this.state.view0.addRef.current.clientHeight  
-        copyOfViews.map((view, index) => {
+  
+        copyOfViews.forEach((view, index) => {
             view.verticalPosition=view.addRef.current.getBoundingClientRect().y;
         })
         
         this.setState({views:copyOfViews}, ()=>{
           
-             this.state.views.map((view, index)=>{
+             this.state.views.forEach((view, index)=>{
                  if(index!==1){
                     this.setState({[`view${index}`]:view}, ()=> this.setState({view1:this.state.view0})); 
                  }     
             }) 
         });
-
-        
-      
-        /* if(hero.addRef.current.offsetTop+window.pageYOffset>=160){
-            this.setState()
-        } */
-
-        
-        //display nav bar based on scroll position of hero
-        /* if(this.state.view0.addRef.current) */
-       
-        //get the current view
-        //const view = this.state[`view${this.state.currentView}`].addRef.current;
-        //check its position
-        //let position=view.current.offsetTop;
-        //if it has reached the top
-            //changeState
-                //reachedTop:true
-                //verticalPosition
-            //
-            //else
-                //changeState
-                    //verticalPosition
-       /*  this.state.refs.map(ref=>{
-            console.log(ref, ref.current.offsetTop,  window.pageYOffset );
-
-        })  */
-        const workingView=this.state.refs[0].current;
-        
-        //console.log(workingView, workingView.offsetTop+window.pageYOffset ) 
-        /* workingView.offsetTop-(window.innerHeight)+window.pageYOffset */
       } 
 
   render() {
