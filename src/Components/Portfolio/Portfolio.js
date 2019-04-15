aa950051e57bb64b6df72ddcb4b2ca0854c5aa36
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-15 10:14:38 +0200

    portfolio renders the widgets directly instead of the carousel

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./Portfolio.module.scss";
-//import ProjectWidget from './ProjectWidget/ProjectWidget';
+import ProjectWidget from './ProjectWidget/ProjectWidget';
 import Carousel from './Carousel/Carousel';
 //import PropTypes from 'prop-types';
 import content from '../../content';
@@ -10,10 +10,23 @@ import content from '../../content';
 
 const Portfolio = React.forwardRef((props,ref)=>(
   <div className={styles.main} ref={ref}>
-    <h2>{props.title? props.title:"Title goes here"}</h2>
-    <Carousel
-      content={content.portfolio}
-    />
+    <ul className={styles.projectList}>
+    {content.portfolio.map((project, index)=>(
+        <li key={project.id}>
+           <ProjectWidget
+          
+          title={project.title}
+          image={project.image}
+          alt={project.alt}
+          description={project.description}
+          />
+
+        </li>
+         
+        ))}
+
+    </ul>
+     
   </div>
   
 
