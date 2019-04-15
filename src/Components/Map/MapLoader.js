e877f8566af0d17e82c43a6701cfa0ca5f2c7932
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-15 10:13:34 +0200

    changed: changed map style

--- src/Components/Map/MapLoader.js
+++ src/Components/Map/MapLoader.js
@@ -65,7 +65,7 @@ class Map extends Component {
                     "elementType": "geometry",
                     "stylers": [
                       {
-                        "color": "#212121"
+                        "color": "#f5f5f5"
                       }
                     ]
                   },
@@ -81,7 +81,7 @@ class Map extends Component {
                     "elementType": "labels.text.fill",
                     "stylers": [
                       {
-                        "color": "#757575"
+                        "color": "#616161"
                       }
                     ]
                   },
@@ -89,42 +89,25 @@ class Map extends Component {
                     "elementType": "labels.text.stroke",
                     "stylers": [
                       {
-                        "color": "#212121"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "administrative",
-                    "elementType": "geometry",
-                    "stylers": [
-                      {
-                        "color": "#757575"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "administrative.country",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
-                      {
-                        "color": "#9e9e9e"
+                        "color": "#f5f5f5"
                       }
                     ]
                   },
                   {
                     "featureType": "administrative.land_parcel",
-                    "stylers": [
-                      {
-                        "visibility": "off"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "administrative.locality",
                     "elementType": "labels.text.fill",
                     "stylers": [
                       {
                         "color": "#bdbdbd"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "poi",
+                    "elementType": "geometry",
+                    "stylers": [
+                      {
+                        "color": "#eeeeee"
                       }
                     ]
                   },
@@ -142,7 +125,7 @@ class Map extends Component {
                     "elementType": "geometry",
                     "stylers": [
                       {
-                        "color": "#181818"
+                        "color": "#e5e5e5"
                       }
                     ]
                   },
@@ -151,75 +134,21 @@ class Map extends Component {
                     "elementType": "labels.text.fill",
                     "stylers": [
                       {
-                        "color": "#616161"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "poi.park",
-                    "elementType": "labels.text.stroke",
-                    "stylers": [
-                      {
-                        "color": "#1b1b1b"
+                        "color": "#9e9e9e"
                       }
                     ]
                   },
                   {
                     "featureType": "road",
-                    "elementType": "geometry.fill",
+                    "elementType": "geometry",
                     "stylers": [
                       {
-                        "color": "#2c2c2c"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "road",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
-                      {
-                        "color": "#8a8a8a"
+                        "color": "#ffffff"
                       }
                     ]
                   },
                   {
                     "featureType": "road.arterial",
-                    "elementType": "geometry",
-                    "stylers": [
-                      {
-                        "color": "#373737"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "road.highway",
-                    "elementType": "geometry",
-                    "stylers": [
-                      {
-                        "color": "#3c3c3c"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "road.highway.controlled_access",
-                    "elementType": "geometry",
-                    "stylers": [
-                      {
-                        "color": "#4e4e4e"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "road.local",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
-                      {
-                        "color": "#616161"
-                      }
-                    ]
-                  },
-                  {
-                    "featureType": "transit",
                     "elementType": "labels.text.fill",
                     "stylers": [
                       {
@@ -228,11 +157,56 @@ class Map extends Component {
                     ]
                   },
                   {
+                    "featureType": "road.highway",
+                    "elementType": "geometry",
+                    "stylers": [
+                      {
+                        "color": "#dadada"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "road.highway",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      {
+                        "color": "#616161"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "road.local",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                      {
+                        "color": "#9e9e9e"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "transit.line",
+                    "elementType": "geometry",
+                    "stylers": [
+                      {
+                        "color": "#e5e5e5"
+                      }
+                    ]
+                  },
+                  {
+                    "featureType": "transit.station",
+                    "elementType": "geometry",
+                    "stylers": [
+                      {
+                        "color": "#eeeeee"
+                      }
+                    ]
+                  },
+                  {
                     "featureType": "water",
                     "elementType": "geometry",
                     "stylers": [
                       {
-                        "color": "#000000"
+                        "color": "#c9c9c9"
                       }
                     ]
                   },
@@ -241,7 +215,7 @@ class Map extends Component {
                     "elementType": "labels.text.fill",
                     "stylers": [
                       {
-                        "color": "#3d3d3d"
+                        "color": "#9e9e9e"
                       }
                     ]
                   }
