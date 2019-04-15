c943c282de8d9f7b93a727f048c4d3dcaf7527ec
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-15 10:15:38 +0200

    removed outter div

--- src/Components/Portfolio/ProjectWidget/ProjectWidget.js
+++ src/Components/Portfolio/ProjectWidget/ProjectWidget.js
@@ -8,11 +8,11 @@ import styles from "./ProjectWidget.module.scss";
 const ProjectWidget =(props)=>(
   <div className={styles.main}>
     <h3>{props.title}</h3>
-    <div className={styles.projectImg}>
+{/*     <div className={styles.projectImg}> */}
         <div className={styles.imgCont}>
             <img className={styles.img} src={props.image} alt={props.alt}/>  
         </div>
-    </div>
+  {/*   </div> */}
     <div className={styles.description}>
         {props.description}
         <button className={styles.btn}>Open in new window</button>
