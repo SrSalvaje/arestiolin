6c3e8e97780eb2c007ab1626bae996cb4c4fcd72
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-16 10:19:00 +0200

    feat: focus is added based ons croll position of views

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,17 +5,26 @@ import styles from "./Nav.module.scss";
 
 
 
-const Nav = React.forwardRef((props, ref)=>(
-    <ul className={(props.verticalPosition<=-80 )?styles.main: styles.hide} ref={ref}>
-        {props.links.map(link=>(
-            <li key={link.name} className={styles.links} onClick={()=>{
-                window.scrollTo(0,`${(window.innerHeight+100)*link.position}`); 
-            }}>{link.name}</li>
-        ))}
-    </ul>
-  
+const Nav = React.forwardRef((props, ref)=>{
+   
+    const goTo=(e, position)=>{
+        e.preventDefault();
+        e.target.className=styles.linksCurrent;
+        window.scrollTo(0,`${(window.innerHeight+100)*position}`);   
+    }
 
-));
+
+    return(
+        <nav className={(props.verticalPosition<=-80 )?styles.main: styles.hide} ref={ref}>
+        <ul>
+        {props.links.map(link=>(
+           <li key={link.name} ><a  href={`#${link.name}`} className={link.name===props.currentView? styles.linksCurrent: styles.links}  onClick={(e)=> goTo(e, link.position)}>{link.name}</a></li>
+        ))}
+        </ul>
+    </nav>
+
+    )
+    });
 
 
 
