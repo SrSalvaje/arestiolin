231016745f649cd98dc5cd170cbb0ec7abb54f00
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-18 22:26:42 +0200

    styled modal

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from "./Modal.module.scss";
+import ResponsiveImg from '../RespLogo/RespLogo';
+import myface from '../../Assets/img/myFace.jpeg'
 //import PropTypes from 'prop-types';
 
 
@@ -10,10 +12,11 @@ const Modal = (props)=>{
         <div className={props.isModal?styles.display:styles.modal}>
           
             <div className={styles.modalContent}>
-                <p className={styles.close} onClick={props.openModal}>&times;</p>
-                <p>Jon Miren Aresti Olin</p>
-                <p>jon.m.h.aresti@gmail.com</p>
-                <p>(+46)763218280</p>
+                <span className={styles.close} onClick={props.openModal}>&times;</span>
+                <ResponsiveImg animation={styles.pictureShadow}logo={myface} alt={"A Picture of me and my dogs."} mainDivStyles={styles.myPicture} />
+                <p className={styles.myName}>Jon Miren Aresti Olin</p>
+                <p className={styles.myEmail}>jon.m.h.aresti@gmail.com</p>
+                <p className={styles.myPhone}>(+46)763218280</p>
             </div>
         </div>
         
