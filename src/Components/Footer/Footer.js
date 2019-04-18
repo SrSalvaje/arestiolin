856f72b75ef4da84cb90159da487b838cf60cc8f
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-18 11:51:58 +0200

    feat: added react logo

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from "./Footer.module.scss";
 import RespLogo from '../RespLogo/RespLogo';
 import google from '../../Assets/img/google.png';
-import ReactLogo from '../../Assets/img/svg/logo.svg'
+import ReactLogo from '../../Assets/img/svg/logo.svg';
 //import PropTypes from 'prop-types';
 
 
@@ -14,12 +14,12 @@ const Footer = (props)=>{
 
 
     return(
-        <div className={styles.main}>
+        <div className={styles.main}>         
             <p onClick={props.openModal}>Created by Jon Miren</p>
             <div className={styles.attribution}>Icons by <a href="https://www.flaticon.com/authors/smalllikeart"  target={"_blank"} rel={"noopener"} title="smalllikeart">smalllikeart</a> @ <a href="https://www.flaticon.com/"  target={"_blank"} rel={"noopener"}title="Flaticon">www.flaticon.com</a></div>
             <div className={styles.logos}>
                 <RespLogo mainDivStyles={styles.mainDivStyles} logo={google}/>
-                <RespLogo mainDivStyles={styles.mainDivStyles} logo={ReactLogo} animation={styles.reactLogo}/>
+                <RespLogo mainDivStyles={styles.mainDivStyles} logo={ReactLogo} animation={styles.reactLogo}/>           
             </div>
             
         </div>
