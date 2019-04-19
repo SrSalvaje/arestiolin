2705d6787adaaf8598ac47f0e0775160c4675d93
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-19 11:54:58 +0200

    breakpoint to 768

--- src/Components/CV/List/List.js
+++ src/Components/CV/List/List.js
@@ -4,7 +4,7 @@ import styles from './List.module.scss';
 const List = (props) => (
     <div className={props.position<=350? styles.displayList : styles.listCont }>
         <ul className={styles.employmentList}>
-        {Object.keys(props.items).map((key)=>(<li onClick={(ev)=>props.openInfo(ev.target)} id= {props.items[key].id} key={props.items[key].id} className={styles.listItem}>{props.items[key].title}</li> ))}
+        {Object.keys(props.items).map((key)=>(<li onClick={(ev)=>props.openInfo(ev.currentTarget)} id= {props.items[key].id} key={props.items[key].id} className={styles.listItem}><span>{props.items[key].title}</span></li> ))}
         </ul>
 
     </div>
