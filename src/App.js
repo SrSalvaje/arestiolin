20a8a3f4e45bd1962dd7c4bf2cc914a845f7ebda
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-22 07:40:53 +0200

    style removed empty space

--- src/App.js
+++ src/App.js
@@ -79,10 +79,8 @@ class App extends Component {
     return (
    
         <div className={styles.App} onScrollCapture={e=>{this.handlesScroll(e)}}>
-         <div style={{ backgroundImage: `url( ${bc} )`}} className={styles.heroImg}></div>
-        
-         
-          {/* <GetScrollPos getRefs={this.getRefs} setCurrentView={this.setCurrentView}  >     */} 
+          <div style={{ backgroundImage: `url( ${bc} )`}} className={styles.heroImg}></div>
+          <GetScrollPos getRefs={this.getRefs} setCurrentView={this.setCurrentView} >
             <HeroCont
               id={"hero"}
               title={content.hero.title}
@@ -90,7 +88,6 @@ class App extends Component {
               button={content.hero.button}
               background={content.hero.background}
             />
-         
             <Nav
               viewPortHeight={this.state.viewPortHeight}
               viewPortWidth={this.state.viewPortWidth}
@@ -105,15 +102,13 @@ class App extends Component {
               ]}
               openModal={this.openModal}
             />
-                 <div style={{width:'100vw', height:'100vh', overflow:'auto', backgroundColor: 'red'}}>
             <Profile height={this.state.viewPortHeight} width={this.state.viewPortWidth} id={"profile"}/>
             <CV  id={"cv"} />
             <Portfolio id={'portfolio'}/>
-          {/* </GetScrollPos> */}
+         </GetScrollPos>
           <Footer  openModal={this.openModal}></Footer>
           <Modal isModal={this.state.isModal}
           openModal={this.openModal}/>
-          </div>
         </div>
     
     );
