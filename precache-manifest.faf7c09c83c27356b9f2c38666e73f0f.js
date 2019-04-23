b368455184c767b2704aa4085fe6b8648a116023
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-23 22:19:39 +0200

    Updates

---
+++ precache-manifest.faf7c09c83c27356b9f2c38666e73f0f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "970657a4f46c5d338ba3ae5b07ba57f4",
+    "url": "/arestiolin/static/media/restRev.970657a4.JPG"
+  },
+  {
+    "revision": "960b8a44ffd7332dae13",
+    "url": "/arestiolin/static/css/main.25dbe374.chunk.css"
+  },
+  {
+    "revision": "042dcbb3056eb60045b0",
+    "url": "/arestiolin/static/js/runtime~main.9598e85c.js"
+  },
+  {
+    "revision": "fa75251951a0c23b05b1",
+    "url": "/arestiolin/static/js/2.38764a13.chunk.js"
+  },
+  {
+    "revision": "d2bd1f98b6a7d95d7ce117a588584e8c",
+    "url": "/arestiolin/static/media/google.d2bd1f98.png"
+  },
+  {
+    "revision": "fef112edf5368bbbca7793646beeb6f3",
+    "url": "/arestiolin/static/media/diving.fef112ed.svg"
+  },
+  {
+    "revision": "ef9dd36f4eea038de1acff229a2d7a5f",
+    "url": "/arestiolin/static/media/news.ef9dd36f.svg"
+  },
+  {
+    "revision": "620fc071bb137bf588a1c6d72c27466c",
+    "url": "/arestiolin/static/media/sale.620fc071.svg"
+  },
+  {
+    "revision": "d8a914c3cb4fb62ed9841c9a1f2d2af3",
+    "url": "/arestiolin/static/media/zen-space-web.d8a914c3.jpg"
+  },
+  {
+    "revision": "7eb784920044963506ac7a32d61b7105",
+    "url": "/arestiolin/static/media/myMalmo.7eb78492.JPG"
+  },
+  {
+    "revision": "bae73f78576f4143bee9b32e07790a66",
+    "url": "/arestiolin/static/media/myReads.bae73f78.JPG"
+  },
+  {
+    "revision": "960b8a44ffd7332dae13",
+    "url": "/arestiolin/static/js/main.1bda4606.chunk.js"
+  },
+  {
+    "revision": "c4d348e033806271c58bfbd82980df50",
+    "url": "/arestiolin/static/media/matching.c4d348e0.JPG"
+  },
+  {
+    "revision": "b57e0d2a6a6c64f814ed93bb72de9277",
+    "url": "/arestiolin/static/media/arcade.b57e0d2a.JPG"
+  },
+  {
+    "revision": "1bba3da1d341e819898cbf9e80e10f76",
+    "url": "/arestiolin/static/media/coding.1bba3da1.png"
+  },
+  {
+    "revision": "e61a277b2f787b87fb05bd45939fce36",
+    "url": "/arestiolin/static/media/teach.e61a277b.svg"
+  },
+  {
+    "revision": "ff4f0745acc418f299474fee07b15a85",
+    "url": "/arestiolin/static/media/jasmine.pg.ff4f0745.jpg"
+  },
+  {
+    "revision": "09bcacde630a1ad2f1e276a5d5fbf2ac",
+    "url": "/arestiolin/static/media/signs.09bcacde.png"
+  },
+  {
+    "revision": "c9c09d0aa3ce2d5f2af07de879fe0c3e",
+    "url": "/arestiolin/static/media/hero.c9c09d0a.JPG"
+  },
+  {
+    "revision": "242fb8cace82caf1d71cfc4226eaf4c4",
+    "url": "/arestiolin/static/media/myFace.242fb8ca.jpeg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/arestiolin/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "b87ae92c3c67d9b633476d52f0381b5b",
+    "url": "/arestiolin/index.html"
+  }
+];
