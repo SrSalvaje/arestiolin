3a6d6da2889c767f7dbcfedbd3b74b49aab603c2
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-24 15:01:01 +0200

    refactor: router and hide/show nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route} from "react-router-dom";
 import styles from './App.module.scss';
 import content from './content'
 import HeroCont from './Components/Hero/HeroCont';
@@ -7,33 +8,55 @@ import CV from './Components/CV/CV';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Nav from './Components/Nav/Nav';
 import bc from './Assets/img/hero.JPG';
-import GetScrollPos from './HOCS/GetScrollPos/GetScrollPos';
+
 import Modal from './Components/Modal/Modal';
-import  {debounce}from 'lodash';
+import  {debounce, throttle}from 'lodash';
 import Footer  from "./Components/Footer/Footer";
+
+
+let prevScroll = window.pageYOffset;
 
 class App extends Component {
   state={
-    refs:[],
+  
     currentView:"",
+    showNav:true,
     isModal:false,
     viewPortHeight:"",
     viewPortWidth:"",
+    openedWidget:""
+
   
+  }
+
+  clickWidget=(widget)=>{  
+    this.setState(function(prevState){
+      return {openedWidget: prevState.openedWidget===widget?"":widget};
+    })
   }
 
   componentDidMount(){
     this.getViewpoertSize();
     window.addEventListener("resize", debounce(this.getViewpoertSize, 500));
+    window.addEventListener('scroll', throttle(this.showNav, 200));
   }
 
   componentWillUnmount(){
     window.removeEventListener("resize", debounce(this.getViewpoertSize, 500));
+    window.removeEventListener('scroll', throttle(this.showNav, 200));
   }
 
- /*  shouldComponentUpdate(){
+  showNav=()=>{
+    let currentScroll= window.pageYOffset;
+    if(prevScroll > currentScroll){
+      this.setState({showNav:true})
+    }else{
+      this.setState({showNav:false})
+    }
+    prevScroll=currentScroll;
+  }
 
-  } */
+  
 
   getViewpoertSize=()=>{
     this.setState({viewPortHeight:window.innerHeight, viewPortWidth:window.innerWidth});   
@@ -44,66 +67,48 @@ class App extends Component {
      return {isModal:!prevState.isModal}
   })
 }
-  getRefs=(views)=>{
-    this.setState({refs:views.slice(0)});
-  }
 
-  setCurrentView=()=>{
-    let currentView="";
-    this.state.refs.forEach((view, index)=>{
-      if(index===0 && view.verticalPosition>=400){
-        currentView="hero"; 
-   
-      }
-      else if(index===2 && view.verticalPosition<=200){
-        currentView="profile";
-       
-      }
-      else if(index===3 && view.verticalPosition<=200){
-        currentView="cv";
-       
-      }
-      else if(index===4 && view.verticalPosition<=200){
-       currentView="portfolio";
-      
-      }
-    });
-    this.setState({currentView:currentView});
-  }
+
   clickedOnNav=(currentView)=>{
     this.setState({currentView:currentView})
   }
+
   render() {
     return (
    
-        <div className={styles.App}>
+        <div className={styles.App} >
           <div style={{ backgroundImage: `url( ${bc} )`}} className={styles.heroImg}></div>
-          <GetScrollPos getRefs={this.getRefs} setCurrentView={this.setCurrentView} >
-            <HeroCont
-              id={"hero"}
-              title={content.hero.title}
-              content={content.hero.content}
-              button={content.hero.button}
-              background={content.hero.background}
-            />
+          
             <Nav
+              showNav={this.state.showNav}
               viewPortHeight={this.state.viewPortHeight}
               viewPortWidth={this.state.viewPortWidth}
               clickedOnNav={this.clickedOnNav}
               id={"nav"}
               currentView={this.state.currentView}
               links={[
-              {name:'profile', position: 1, elementRef:this.state.refs[2]},
-              {name:'cv', position: 2, elementRef:this.state.refs[3]},
-              {name:'portfolio', position: 3, elementRef:this.state.refs[4]},
-              {name:'contact', position:4, }
+              {name:'profile', position: 1},
+              {name:'cv', position: 2},
+              {name:'portfolio', position: 3},
+              {name:'contact', position:4 }
               ]}
               openModal={this.openModal}
             />
-            <Profile height={this.state.viewPortHeight} width={this.state.viewPortWidth} id={"profile"}/>
-            <CV  id={"cv"}/>
-            <Portfolio id={'portfolio'} width={this.state.viewPortWidth}/>
-         </GetScrollPos>
+          <Route path='/profile' render={()=>(
+            <React.Fragment>
+              <HeroCont
+              id={"hero"}
+              title={content.hero.title}
+              content={content.hero.content}
+              button={content.hero.button}
+              background={content.hero.background}
+            />
+            <Profile height={this.state.viewPortHeight} width={this.state.viewPortWidth} id={"profile"} openedWidget={this.state.openedWidget} clickWidget={this.clickWidget}/>
+            </React.Fragment>
+          )}
+          />
+          <Route path='/cv' render={()=>( <CV  id={"cv"}/>)}/>
+          <Route path='/portfolio' render={()=>(<Portfolio id={'portfolio'} width={this.state.viewPortWidth}/>)}/>
           <Footer  openModal={this.openModal}></Footer>
           <Modal isModal={this.state.isModal}
           openModal={this.openModal}/>
