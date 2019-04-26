ac67ab131f321ee268d9f57aa6db410d4322b01f
Jon Miren Aresti Olin <jon.m.h.aresti@gmail.com>
2019-04-26 20:27:44 +0200

    added scroll affordance arrow

--- src/Components/Hero/HeroCont.js
+++ src/Components/Hero/HeroCont.js
@@ -10,11 +10,30 @@ const HeroCont = (props)=>(
     <header id={'home'} className={styles.heroCont} >
         {/* <div style={{ backgroundImage: `url( ${bc} )`}} className={styles.heroImg} > 
         </div> */}
+       
+            
+           
         <div className={styles.heroTxt}>
-        <h1>{props.title}</h1>
-        <p>{props.content}</p>
-        <p className={styles.heroButton}>{props.button}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
+            <p className={styles.heroButton}>{props.button}</p>
+            <div className={styles.arrowAnim}>
+            <div className={styles.arrowSliding}>
+                <div className={styles.arrow}></div>
+            </div>
+            <div className={[styles.arrowSliding, styles.delay1].join(" ")}>
+                <div className={styles.arrow}></div>
+            </div>
+            <div className={[styles.arrowSliding, styles.delay2].join(" ")}>
+                <div className={styles.arrow}></div>
+            </div>
+            <div className={[styles.arrowSliding, styles.delay3].join(" ")}>
+                <div className={styles.arrow}></div>
+            </div>
+            </div> 
+        
         </div>
+       
     </header>
 
 );
